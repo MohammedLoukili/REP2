256cd9885821ee71916f4fd4dbc1f0587b61e155
MohammedLoukili <mloukili@gmail.com>
2022-07-11 15:32:40 +0200

    étape 2

--- src/ma/inset/test.java
+++ src/ma/inset/test.java
@@ -1,6 +1,7 @@
 package  ma.inset ;
 public class  test {
  public static void main(String[] args) {
-    System.out.println("bonjour tout le monde");
+    String s= new String(" Mohammed");
+    System.out.println("bonjour" + s);
  }
 }
